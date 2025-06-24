945a6e9b0e111b7e4355fb41d573b90adba9f126
JavidPack <javidpack@gmail.com>
2025-06-23 18:17:44 -0600

    Fix LongMoss missing from TileID.Sets.IgnoredByGrowingSaplings
    Co-Authored-By: CactusDuper <58598131+cactusduper@users.noreply.github.com>

--- patches/tModLoader/Terraria/ID/TileID.TML.cs
+++ patches/tModLoader/Terraria/ID/TileID.TML.cs
@@ -104,7 +104,7 @@ partial class TileID
 		public static bool[] CountsAsShimmerSource = Factory.CreateBoolSet();
 
 		/// <summary> Whether or not saplings count this tile as empty when trying to grow. </summary>
-		public static bool[] IgnoredByGrowingSaplings = Factory.CreateBoolSet(3, 24, 32, 61, 62, 69, 71, 73, 74, 82, 83, 84, 110, 113, 201, 233, 352, 485, 529, 530, 637, 655);
+		public static bool[] IgnoredByGrowingSaplings = Factory.CreateBoolSet(3, 24, 32, 61, 62, 69, 71, 73, 74, 82, 83, 84, 110, 113, 184, 201, 233, 352, 485, 529, 530, 637, 655);
 
 		/// <summary> Whether or not this tile prevents a meteor from landing near it.
 		/// <para/> Contains LihzahrdBrick, DisplayDoll, HatRack, FallenLog, and TeleportationPylon.
