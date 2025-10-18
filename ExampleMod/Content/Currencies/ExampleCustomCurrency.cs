b9b4ddc753115cdd5dadb3e9664339369a45325b
Mirsario <me@mirsar.io>
2025-10-18 13:55:07 +0300

    examples: Improved ExampleCustomCurrency implementation.

--- ExampleMod/Content/Currencies/ExampleCustomCurrency.cs
+++ ExampleMod/Content/Currencies/ExampleCustomCurrency.cs
@@ -1,15 +1,40 @@
 ﻿using Microsoft.Xna.Framework;
 using Terraria.GameContent.UI;
+using Terraria.ModLoader;
 
 namespace ExampleMod.Content.Currencies
 {
 	// An example of a custom currency, similar to the Defenders Medal.
-	// Note that the code in ExampleMod.cs is required to register this custom currency. In that code you can see that ExampleItem is assigned as the item used for this currency, there is no "ExampleCustomCurrency" item.
-	public class ExampleCustomCurrency : CustomCurrencySingleCoin
+	// This class implements the ILoadable interface, meaning that its parameterless constructor will be used
+	// to auto-register an instance of it into our mod, automatically invoking Load() and Unload().
+	public class ExampleCustomCurrency : CustomCurrencySingleCoin, ILoadable
 	{
-		public ExampleCustomCurrency(int coinItemID, long currencyCap, string CurrencyTextKey) : base(coinItemID, currencyCap) {
-			this.CurrencyTextKey = CurrencyTextKey;
-			CurrencyTextColor = Color.BlueViolet;
+		// A static property where we store the ID of our currency.
+		// This is what Item.shopSpecialCurrency will be set to to make a shop entry use our currency.
+		public static int CurrencyId { get; private set; }
+
+		// This constructor is used by ILoadable, it is not to be used by anything else.
+		private ExampleCustomCurrency() : base(-1, -1) { }
+
+		public ExampleCustomCurrency(int coinItemId, long currencyCap, string currencyTextKey, Color currencyTextColor) : base(coinItemId, currencyCap) {
+			CurrencyTextKey = currencyTextKey;
+			CurrencyTextColor = currencyTextColor;
 		}
+
+		// This is the explicit implementation syntax for interface methods, it lets us make a private method that is still callable by interfaces.
+		void ILoadable.Load(Mod mod) {
+			// This call actually registers our custom currency.
+			CurrencyId = CustomCurrencyManager.RegisterCurrency(new ExampleCustomCurrency(
+				// We assign ExampleItem as the item used for this currency, there is no "ExampleCustomCurrency" item.
+				coinItemId: ModContent.ItemType<Items.ExampleItem>(),
+				// The currency is the max amount of held currency that will be counted and displayed.
+				// Item prices in shops should not be higher than this value if you want them to be ever affordable.
+				currencyCap: 999,
+				// Localization string and color that are used for this currency's name.
+				currencyTextKey: "Mods.ExampleMod.Currencies.ExampleCustomCurrency",
+				currencyTextColor: Color.BlueViolet
+			));
+		}
+		void ILoadable.Unload() { }
 	}
 }
